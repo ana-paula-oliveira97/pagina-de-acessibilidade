6b6749913a944068fa9839fb2010da597b07f403
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-12-05 18:40:42 -0300

    modo escuro

---
+++ js/modo-escuro.js
@@ -0,0 +1,39 @@
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) =>
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
+}
+
+const darkMode = {
+    bg: "#0f0f0f",
+    bgPanel: "#242323",
+    colorHeadings: "#3664FF",
+    colorText: "#ffffff",
+    hover: "#40edce",
+    rodape: "#000000"
+}
+
+const transformKey = key =>
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key =>
+        html.style.setProperty(transformKey(key), colors[key])
+    )
+}
+
+
+checkbox.addEventListener("change", ({ target }) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
