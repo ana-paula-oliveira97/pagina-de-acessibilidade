deb5032e9227a0850233aa4536ca066d46f9e469
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-12-06 16:30:29 -0300

    fontes

--- js/zoom.js
+++ js/zoom.js
@@ -1,15 +1,28 @@
-function fonte(e) {
-    var elemento = document.getElementById("conteudo");
-    var atual = elemento.estilo.fontSize;
-    document.getElementById("backup").value = back;
-    back = parseInt(atual);
+window.onload = function () {
+    var elementBody = document.querySelector('body');
+    var elementBtnIncreaseFont = document.getElementById('increase-font');
+    var elementBtnDecreaseFont = document.getElementById('decrease-font');
+    var elementBtnNormalFont = document.getElementById('normal-font');
+    // Padrão de tamanho, equivale a 100% do valor definido no Body
+    var fontSize = 100;
+    // Valor de incremento ou decremento, equivale a 10% do valor do Body
+    var increaseDecrease = 5;
 
-    if (e === 'a') {
-        atual = parseInt(atual) + 2 + 'px';
-    } else if (e === 'b') {
-        atual = parseInt(atual) - 2 + 'px';
-    } else if (e === 'c') {
-        atual = 19 + 'px';
-    }
-    elemento.estilo.fontSize = atual;
+    // Evento de click para aumentar a fonte
+    elementBtnIncreaseFont.addEventListener('click', function (event) {
+        fontSize = fontSize + increaseDecrease;
+        elementBody.style.fontSize = fontSize + '%';
+    });
+
+    // Evento de click para diminuir a fonte
+    elementBtnDecreaseFont.addEventListener('click', function (event) {
+        fontSize = fontSize - increaseDecrease;
+        elementBody.style.fontSize = fontSize + '%';
+    });
+
+
+    elementBtnNormalFont.addEventListener('click', function (event) {
+        fontSize = fontSize;
+        elementBody.style.fontSize = fontSize;
+    });
 }
